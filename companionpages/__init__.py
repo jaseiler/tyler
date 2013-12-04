8538e8adb4d931caed4ca3d4466d3354f6fafc30
Jennifer Seiler <jese@aei.mpg.de>
2013-12-04 15:35:32 -0700

    again

--- companionpages/__init__.py
+++ companionpages/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
-__author__ = 'Tyler Contributors'
-__email__ = 'tyler@codersquid.com'
-__version__ = '0.11.0'
-=======
 __author__ = 'Research Compendia Contributors'
 __email__ = 'researchcompendia@gmail.com'
 __version__ = '1.0.0-beta2'
->>>>>>> upstream/master
